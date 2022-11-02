f57457f40933513f376c754b638bd69ac57c4f91
Mikhail Holubeu <mikhail.holubeu@sap.com>
2022-11-02 21:34:48 +0300

    Add base logic

---
+++ qr-powerpoint-add-in/qr-powerpoint-add-in/QrCodeProcessor.cs
@@ -0,0 +1,16 @@
+﻿using IronBarCode;
+
+namespace qr_powerpoint_add_in
+{
+    public class QrCodeProcessor
+    {
+        public static void ConvertUrlToQrCode(string url)
+        {
+            GeneratedBarcode qrCode = QRCodeWriter.CreateQrCode(url);
+
+            qrCode.SaveAsHtmlFile("qrCode.html");
+
+            System.Diagnostics.Process.Start("qrCode.html");
+        }
+    }
+}
---
+++ qr-powerpoint-add-in/qr-powerpoint-add-in/Ribbon1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System.Drawing;
+
+namespace qr_powerpoint_add_in
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            string text = editBox1.Text;
+            QrCodeProcessor.ConvertUrlToQrCode(text);
+            label1.Label = text;
+            //QrCodeProcessor.ConvertUrlToQrCode(text);
+        }
+    }
+}
